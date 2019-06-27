9075f43e17806a11148f186c7aa81bf3a1d658b2
Miles Standoffish <toddnan@gmail.com>
2019-06-27 08:28:49 -0500

    added responses for correct & wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,13 @@
 // the correct answers are preceded by an '*'
 
-let questionTimer = "";
+let questionIntervalId = ""; // for canceling the timer once the player selects an answer
+let questionIndex = 0; // for tracking which question we're on
+const questionTime = 5; // time question is shown, in seconds
+const answerTime = 3; // time answer is shown, in seconds
+let secondsLeft = 0; // counts down to zero
+let correctAnswers = 0;
+let wrongAnswers = 0;
+let timedOutAnswers = 0;
 
 
 let questionBank = [
@@ -37,33 +44,39 @@ let questionBank = [
 ];
 
 function initGame() {
-    $("#answers").empty();
-    $("#answers").html('<div class="button" id="start">Start</div>');
+    $("#answers-area").hide();
+    $("#start").show();
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    timedOutAnswers = 0;
+    questionIndex = 0;
 }
 
 function startGame() {
-    $("#answers").empty(); // to remove Start button
-    questionBank.forEach(presentQuestion);
+    $("#start").hide();
+    $("#answers-area").show();
+    presentQuestion();
 }
 
-function presentQuestion(q) {
-    console.log("Q: " + q.question);
-    $("#question").text(q.question);
-
+function presentQuestion() {
+    //console.log("Q: " + q.question);
+    console.log("presentQuestion");
+    secondsLeft = questionTime;
     let order = randomOrder();
-    for (let i=0; i<order.length; i++) {
-        let answer = q.answers[order[i]];
-        let $btn = $("<div>");
-        $btn.addClass("button answer");
-        $btn.attr("data-correct", answer.includes("*"));
-        $btn.text(answer.replace("*", "")); // trim the leading * from correct answers
-        $("#answers").append($btn);
+    $("#question").text(questionBank[questionIndex].question);
 
-        
-    }
-    $(".answer").click(checkAnswer);
-    questionTimer = setTimeout(checkAnswer, 5000);
+    $(".answer").each(function(index, element) {
+        //console.log(element);
+        //console.log(index);
+        let answer = questionBank[questionIndex].answers[order[index]];
+        $(this).text(answer.replace("*","")); // removes * from the correct answer
+        $(this).attr("data-correct", answer.includes("*"));
+    }); 
+
+    $("#communication").text("Time remaining: " + secondsLeft + " seconds");
+    questionTimer = setInterval(updateTimer, 1000);
     
+
 
 }
 
@@ -72,22 +85,51 @@ function randomOrder() {
     return ("3210");
 }
 
+function updateTimer() {
+    secondsLeft--;
+    $("#communication").text("Time remaining: " + secondsLeft + " seconds");
+    if (secondsLeft <= 0) {
+        clearInterval(questionTimer); // stop timer countdown
+        $('#communication').text("Out of time!");
+        showAnswer();
+    }
+}
+
 function checkAnswer () {
     clearTimeout(questionTimer);
 
     console.log("checkAnswer");
-    if (this.attr("data-correct") === true) {
+  
+    if ($(this).attr("data-correct") === 'true') {
         console.log("Correct!");
+        $("#communication").text("Correct!");
     }
     else {
         console.log("Wrong!");
+        $("#communication").text("Wrong! The correct answer is " + $('.answer[data-correct="true"]').text());
     }
+    showAnswer();
+}
 
+function showAnswer() {
+    
+    if (questionIndex === questionBank.length-1) {
+        showScore();
+    } else {
+        questionIndex++; //next question
+        setTimeout(presentQuestion, answerTime*1000);
+    }
+}
 
+function showScore() {
+    //show totals from the game
+
+    $("#start").show();
 }
 
 window.onload = function() {
     $("#start").click(startGame);
+    $(".answer").click(checkAnswer);
 }
 
 
